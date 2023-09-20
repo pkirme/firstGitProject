bce0b7ce41b6c9bb44c0bac42e0e889ee3db95fb
pkirme <kirmepriti@gmail.com>
2023-09-20 15:37:22 +0530

    changes on list group item

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 console.log("I love Git.");
+
 let getHeader=document.getElementById('main-header');
 getHeader.style.borderBottom=("solid 3px black");
-let getItom =document.getElementsByClassName("title")[0];
-getItom.style.fontWeight=("bold");
-getItom.style.color=("green");
+
+let getItem =document.getElementsByClassName("title")[0];
+getItem.style.fontWeight=("bold");
+getItem.style.color=("green");
+
+let item = document.getElementsByClassName("list-group-item");
+console.log(item);
+item[1].textContent="Hello 2";
+item[1].style.fontWeight='bold';
+item[1].style.backgroundColor="yellow";
+for(let i=0;i<item.length;i++){
+    item[i].style.color='blue';
+}
