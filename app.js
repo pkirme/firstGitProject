1afea558c6c0d2f7711bb71e6dcda7b68925aa77
pkirme <kirmepriti@gmail.com>
2023-09-20 15:07:48 +0530

    changes had done by using getElementById ang getElementByClassName.

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("I love Git.");
+let getHeader=document.getElementById('main-header');
+getHeader.style.borderBottom=("solid 3px black");
+let getItom =document.getElementsByClassName("title")[0];
+getItom.style.fontWeight=("bold");
+getItom.style.color=("green");
