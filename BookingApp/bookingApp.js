05d53c100f5ae17bc6ad92cfd0cecd20294905f2
pkirme <kirmepriti@gmail.com>
2023-09-22 16:12:32 +0530

    Adding edit button

--- BookingApp/bookingApp.js
+++ BookingApp/bookingApp.js
@@ -28,6 +28,7 @@ function showUser(userObj){
     li.appendChild(document.createTextNode(userObj.userName+' '+userObj.emailId+' '+userObj.phone));
     userList.appendChild(li);  
 
+    //create delete button.
     let delBtn = document.createElement('button');
     delBtn.className='btn btn-primary btn-sm float-right delete';
     delBtn.appendChild(document.createTextNode('Delete'));
@@ -37,6 +38,20 @@ function showUser(userObj){
         userList.removeChild(li);
     }
     li.appendChild(delBtn);
+
+    //create edit button.
+    let editBtn = document.createElement('button');
+    editBtn.className ='btn btn-primary btn-sm float-right edit';
+    editBtn.appendChild(document.createTextNode('Edit'));
+    editBtn.onclick = () => {
+        document.getElementById('userName').value=userObj.userName;
+        document.getElementById('emailId').value=userObj.emailId;
+        document.getElementById('phNo').value=userObj.phone;
+        localStorage.removeItem(userObj.emailId);
+        userList.removeChild(li);
+    }
+    li.appendChild(editBtn);
+    
     userList.appendChild(li);
 }
 
