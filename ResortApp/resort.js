9f10126e28fb2f50bbd9170aaf49385b079d1968
pkirme <kirmepriti@gmail.com>
2023-11-30 00:21:25 +0530

    code is modified by using async/await.

--- ResortApp/resort.js
+++ ResortApp/resort.js
@@ -1,11 +1,11 @@
 let form=document.getElementById('addForm');
 form.addEventListener('submit',addTable);
-let url='https://crudcrud.com/api/04f451909ec346d6bbd5a0da7b4e4de9/info';
-let table1List=document.getElementById('table1List');
-let table2List=document.getElementById('table2List');
-let table3List=document.getElementById('table3List');
+const url='https://crudcrud.com/api/db50a02b40284e128730898ee34ac301/info';
+// let table1List=document.getElementById('table1List');
+// let table2List=document.getElementById('table2List');
+// let table3List=document.getElementById('table3List');
 
-function addTable(e){
+async function addTable(e){
     e.preventDefault();
     const table={
         price:document.getElementById('price').value,
@@ -13,102 +13,175 @@ function addTable(e){
         tableNo:document.getElementById('select').value
     }
 
-    axios.post(url,table)
-         .then(res=>{
-            console.log(res);
-            location.reload();
-         })
-         .catch(err=>console.log(err));
+    // axios.post(url,table)
+    //      .then(res=>{
+    //         console.log(res);
+    //         location.reload();
+    //      })
+    //      .catch(err=>console.log(err));
+    const response = await axios.post(url, table);
+    const data = response.data;
+    console.log(data);
+    showData(data);
+   
+    //reset all field
+    form.reset()
 }
 
-window.addEventListener("DOMContentLoaded",()=>{
-    axios.get(url)
-         .then(res=>{
-            //console.log(res);
-            for(let i=0;i<res.data.length;i++){
-                console.log(res.data[i]);
-                if(res.data[i].tableNo=='table1'){
-                    showTable1(res.data[i]);
-                }
-                if(res.data[i].tableNo=='table2'){
-                    showTable2(res.data[i]);
-                }
-                if(res.data[i].tableNo=='table3'){
-                    showTable3(res.data[i]);
-                }
-            }
-         })
-         .catch(err=>console.log(err));
+window.addEventListener("DOMContentLoaded",async()=>{
+    // axios.get(url)
+    //      .then(res=>{
+    //         //console.log(res);
+    //         for(let i=0;i<res.data.length;i++){
+    //             console.log(res.data[i]);
+    //             if(res.data[i].tableNo=='table1'){
+    //                 showTable1(res.data[i]);
+    //             }
+    //             if(res.data[i].tableNo=='table2'){
+    //                 showTable2(res.data[i]);
+    //             }
+    //             if(res.data[i].tableNo=='table3'){
+    //                 showTable3(res.data[i]);
+    //             }
+    //         }
+    //      })
+    //      .catch(err=>console.log(err));
+    const response=await axios.get(url);
+    const data=response.data;
+    console.log(data);
+    for(let i=0;i<data.length;i++){
+                    // console.log(getData[i]);
+                    // if(getData[i].tableNo=='table1'){
+                    //     showTable1(getData[i]);
+                    // }
+                    // if(getData[i].tableNo=='table2'){
+                    //     showTable2(getData[i]);
+                    // }
+                    // if(getData[i].tableNo=='table3'){
+                    //     showTable3(getData[i]);
+                    // }
+            showData(data[i]);
+     }
 })
 
-function showTable1(data){
+// function showTable1(data){
+//     let li = document.createElement('li');
+//     li.className='list-group-item';
+//     li.appendChild(document.createTextNode(`${data.dish} : ${data.price}`));
+//     table1List.appendChild(li);  
+
+//     //create delete button.
+//     let delBtn = document.createElement('button');
+//     delBtn.className='btn btn-danger btn-sm float-right delete';
+//     delBtn.appendChild(document.createTextNode('Cancel order'));
+//     //Remove data
+//     delBtn.onclick = async() =>{
+//         // axios.delete(`${url}/${data._id}`)
+//         //      .then(res=>{
+//         //         console.log(res);
+//         //         location.reload();
+//         //      })
+//         //      .catch(err=>console.log(err));
+//         const response=await axios.delete(`${url}/${data._id}`);
+//         console.log(response.data);
+//         table1List.removeChild(li);
+//     }
+//     li.appendChild(delBtn);
+//     table1List.appendChild(li);
+// }
+
+// function showTable2(data){
+//     let li = document.createElement('li');
+//     li.className='list-group-item';
+//     li.appendChild(document.createTextNode(`${data.dish} : ${data.price}`));
+//     table2List.appendChild(li);  
+
+//     //create delete button.
+//     let delBtn = document.createElement('button');
+//     delBtn.className='btn btn-danger btn-sm float-right delete';
+//     delBtn.appendChild(document.createTextNode('Cancel order'));
+//     //Remove data
+//     delBtn.onclick = async() =>{
+//         // axios.delete(`${url}/${data._id}`)
+//         //      .then(res=>{
+//         //         console.log(res);
+//         //         location.reload();
+//         //      })
+//         //      .catch(err=>console.log(err));
+//         const response=await axios.delete(`${url}/${data._id}`);
+//         console.log(response.data);
+//         table2List.removeChild(li);
+//     }
+//     li.appendChild(delBtn);
+//     table2List.appendChild(li);
+// }
+
+// function showTable3(data){
+//     let li = document.createElement('li');
+//     li.className='list-group-item';
+//     li.appendChild(document.createTextNode(`${data.dish} : ${data.price}`));
+//     table3List.appendChild(li);  
+
+//     //create delete button.
+//     let delBtn = document.createElement('button');
+//     delBtn.className='btn btn-danger btn-sm float-right delete';
+//     delBtn.appendChild(document.createTextNode('Cancel order'));
+//     //Remove data
+//     delBtn.onclick = async() =>{
+//         // axios.delete(`${url}/${data._id}`)
+//         //      .then(res=>{
+//         //         console.log(res);
+//         //         location.reload();
+//         //      })
+//         //      .catch(err=>console.log(err));
+//         const response=await axios.delete(`${url}/${data._id}`);
+//         console.log(response.data);
+//         table3List.removeChild(li);
+//     }
+//     li.appendChild(delBtn);
+//     table3List.appendChild(li);
+// }
+
+// //show the table on screen
+// function showData(data) {
+    
+//                         if(data.tableNo=='table1'){
+//                             showTable1(data);
+//                         }
+//                         if(data.tableNo=='table2'){
+//                             showTable2(data);
+//                         }
+//                         if(data.tableNo=='table3'){
+//                             showTable3(data);
+//                         }
+                    
+// }
+
+function showData(data){
+    let table = `${data.tableNo}List`;
+    let tableList=document.getElementById(table);
+    console.log(tableList);
     let li = document.createElement('li');
     li.className='list-group-item';
     li.appendChild(document.createTextNode(`${data.dish} : ${data.price}`));
-    table1List.appendChild(li);  
+   
 
     //create delete button.
     let delBtn = document.createElement('button');
     delBtn.className='btn btn-danger btn-sm float-right delete';
     delBtn.appendChild(document.createTextNode('Cancel order'));
-    //Remove user from local storage
-    delBtn.onclick = () =>{
-        axios.delete(`${url}/${data._id}`)
-             .then(res=>{
-                console.log(res);
-                location.reload();
-             })
-             .catch(err=>console.log(err));
-        table1List.removeChild(li);
+    //Remove data from server
+    delBtn.onclick = async() =>{
+        // axios.delete(`${url}/${data._id}`)
+        //      .then(res=>{
+        //         console.log(res);
+        //         location.reload();
+        //      })
+        //      .catch(err=>console.log(err));
+        const response=await axios.delete(`${url}/${data._id}`);
+        console.log(response.data);
+        tableList.removeChild(li);
     }
     li.appendChild(delBtn);
-    table1List.appendChild(li);
-}
-
-function showTable2(data){
-    let li = document.createElement('li');
-    li.className='list-group-item';
-    li.appendChild(document.createTextNode(`${data.dish} : ${data.price}`));
-    table2List.appendChild(li);  
-
-    //create delete button.
-    let delBtn = document.createElement('button');
-    delBtn.className='btn btn-danger btn-sm float-right delete';
-    delBtn.appendChild(document.createTextNode('Cancel order'));
-    //Remove user from local storage
-    delBtn.onclick = () =>{
-        axios.delete(`${url}/${data._id}`)
-             .then(res=>{
-                console.log(res);
-                location.reload();
-             })
-             .catch(err=>console.log(err));
-        table2List.removeChild(li);
-    }
-    li.appendChild(delBtn);
-    table2List.appendChild(li);
-}
-
-function showTable3(data){
-    let li = document.createElement('li');
-    li.className='list-group-item';
-    li.appendChild(document.createTextNode(`${data.dish} : ${data.price}`));
-    table3List.appendChild(li);  
-
-    //create delete button.
-    let delBtn = document.createElement('button');
-    delBtn.className='btn btn-danger btn-sm float-right delete';
-    delBtn.appendChild(document.createTextNode('Cancel order'));
-    //Remove user from local storage
-    delBtn.onclick = () =>{
-        axios.delete(`${url}/${data._id}`)
-             .then(res=>{
-                console.log(res);
-                location.reload();
-             })
-             .catch(err=>console.log(err));
-        table3List.removeChild(li);
-    }
-    li.appendChild(delBtn);
-    table3List.appendChild(li);
+    tableList.appendChild(li);
 }
