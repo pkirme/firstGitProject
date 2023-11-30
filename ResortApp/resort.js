b335f5aded5f9636213d65eb977e0cf28da44a83
pkirme <kirmepriti@gmail.com>
2023-11-30 11:02:52 +0530

    Removing semicolon.

--- ResortApp/resort.js
+++ ResortApp/resort.js
@@ -1,6 +1,6 @@
-let form=document.getElementById('addForm');
-form.addEventListener('submit',addTable);
-const url='https://crudcrud.com/api/db50a02b40284e128730898ee34ac301/info';
+let form=document.getElementById('addForm')
+form.addEventListener('submit',addTable)
+const url='https://crudcrud.com/api/db50a02b40284e128730898ee34ac301/info'
 
 async function addTable(e){
     e.preventDefault();
@@ -10,43 +10,43 @@ async function addTable(e){
         tableNo:document.getElementById('select').value
     }
 
-    const response = await axios.post(url, table);
-    const data = response.data;
-    console.log(data);
-    showData(data);
+    const response = await axios.post(url, table)
+    const data = response.data
+    console.log(data)
+    showData(data)
    
     //reset all field
     form.reset()
 }
 
 window.addEventListener("DOMContentLoaded",async()=>{
-    const response=await axios.get(url);
-    const data=response.data;
-    console.log(data);
+    const response=await axios.get(url)
+    const data=response.data
+    console.log(data)
     for(let i=0;i<data.length;i++){
-         showData(data[i]);
+         showData(data[i])
      }
 })
 
 function showData(data){
-    const table = `${data.tableNo}List`;
-    const tableList=document.getElementById(table);
+    const table = `${data.tableNo}List`
+    const tableList=document.getElementById(table)
     console.log(tableList);
-    const li = document.createElement('li');
-    li.className='list-group-item';
-    li.appendChild(document.createTextNode(`${data.dish} : ${data.price}`));
+    const li = document.createElement('li')
+    li.className='list-group-item'
+    li.appendChild(document.createTextNode(`${data.dish} : ${data.price}`))
    
 
     //create delete button.
-    const delBtn = document.createElement('button');
-    delBtn.className='btn btn-danger btn-sm float-right delete';
-    delBtn.appendChild(document.createTextNode('Cancel order'));
+    const delBtn = document.createElement('button')
+    delBtn.className='btn btn-danger btn-sm float-right delete'
+    delBtn.appendChild(document.createTextNode('Cancel order'))
     //Remove data from server
     delBtn.onclick = async() =>{
-        const response=await axios.delete(`${url}/${data._id}`);
-        console.log(response.data);
-        tableList.removeChild(li);
+        const response=await axios.delete(`${url}/${data._id}`)
+        console.log(response.data)
+        tableList.removeChild(li)
     }
-    li.appendChild(delBtn);
-    tableList.appendChild(li);
+    li.appendChild(delBtn)
+    tableList.appendChild(li)
 }
