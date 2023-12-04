6a3d16d03417ad6e9572e5aec845ff77c1d3025a
pkirme <kirmepriti@gmail.com>
2023-12-05 02:00:04 +0530

    Creating custom component ExpenceItem

---
+++ react-project/src/components/ExpenceItem.js
@@ -0,0 +1,10 @@
+function ExpenceItem(){
+    return(
+        <div>
+            <li>Food Rs 10</li>
+            <li>Petrol Rs 100</li>
+            <li>Movies Rs 200</li>
+        </div>
+    );
+}
+export default ExpenceItem;
