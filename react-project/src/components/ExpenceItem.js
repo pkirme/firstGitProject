3431a5367a137212e652ce1aaf27f1d8c23eabc2
pkirme <kirmepriti@gmail.com>
2023-12-05 02:03:16 +0530

    adding h2 tag

--- react-project/src/components/ExpenceItem.js
+++ react-project/src/components/ExpenceItem.js
@@ -1,6 +1,7 @@
 function ExpenceItem(){
     return(
         <div>
+            <h2>Expense Items</h2>
             <li>Food Rs 10</li>
             <li>Petrol Rs 100</li>
             <li>Movies Rs 200</li>
