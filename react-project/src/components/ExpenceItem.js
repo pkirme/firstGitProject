c9234b82352ebd3d1a764d6db6eec5d3f0f0c508
pkirme <kirmepriti@gmail.com>
2023-12-05 10:59:02 +0530

    Dynamically adding variables

--- react-project/src/components/ExpenceItem.js
+++ react-project/src/components/ExpenceItem.js
@@ -1,11 +1,27 @@
-function ExpenceItem(){
-    return(
-        <div>
-            <h2>Expense Items</h2>
-            <li>Food Rs 10</li>
-            <li>Petrol Rs 100</li>
-            <li>Movies Rs 200</li>
-        </div>
-    );
+import "./ExpenceItem.css";
+function ExpenceItem() {
+  const LocationOfExpenditure = "Nagpur";
+  return (
+    <div className="expence-item">
+      <div>
+        <h2 className="expense-item__description">Expense Items</h2>
+      </div>
+      <div className="expense-item h2">
+        <h3 className="expense-item__price">
+          Food Rs 10 {LocationOfExpenditure}
+        </h3>
+      </div>
+      <div className="expense-item h2">
+        <h3 className="expense-item__price">
+          Petrol Rs 100 {LocationOfExpenditure}
+        </h3>
+      </div>
+      <div className="expense-item h2">
+        <h3 className="expense-item__price">
+          Movies Rs 200 {LocationOfExpenditure}
+        </h3>
+      </div>
+    </div>
+  );
 }
 export default ExpenceItem;
