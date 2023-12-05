6b79c1627c6d7d2401b2d18b9c38af662f9e3bbf
pkirme <kirmepriti@gmail.com>
2023-12-05 11:32:02 +0530

    Making some changes in css

--- react-project/src/components/ExpenceItem.js
+++ react-project/src/components/ExpenceItem.js
@@ -2,24 +2,38 @@ import "./ExpenceItem.css";
 function ExpenceItem() {
   const LocationOfExpenditure = "Nagpur";
   return (
-    <div className="expence-item">
+    <div>
       <div>
-        <h2 className="expense-item__description">Expense Items</h2>
+        <h2>Expense Items</h2>
       </div>
-      <div className="expense-item h2">
-        <h3 className="expense-item__price">
-          Food Rs 10 {LocationOfExpenditure}
-        </h3>
+      <div className="expense-item">
+        <div>
+          <h3>Food</h3>
+        </div>
+        <div className="expense-item__description">
+          <h2>{LocationOfExpenditure}</h2>
+          <div className="expense-item__price">Rs 10</div>
+        </div>
       </div>
-      <div className="expense-item h2">
-        <h3 className="expense-item__price">
-          Petrol Rs 100 {LocationOfExpenditure}
-        </h3>
+
+      <div className="expense-item">
+        <div>
+          <h3>Petrol</h3>
+        </div>
+        <div className="expense-item__description">
+          <h2>{LocationOfExpenditure}</h2>
+          <div className="expense-item__price">Rs 100</div>
+        </div>
       </div>
-      <div className="expense-item h2">
-        <h3 className="expense-item__price">
-          Movies Rs 200 {LocationOfExpenditure}
-        </h3>
+
+      <div className="expense-item">
+        <div>
+          <h3>Movies</h3>
+        </div>
+        <div className="expense-item__description">
+          <h2>{LocationOfExpenditure}</h2>
+          <div className="expense-item__price">Rs 200</div>
+        </div>
       </div>
     </div>
   );
