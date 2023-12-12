f2b891131716187ba4492636d5e6dfa87198ebdd
pkirme <kirmepriti@gmail.com>
2023-12-12 22:24:28 +0530

    Adding a filter.

--- react-project/src/components/Expenses/Expense.js
+++ react-project/src/components/Expenses/Expense.js
@@ -2,9 +2,20 @@ import "./Expense.css";
 import "./ExpenseItem.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import React, { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
 const Expense = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expense">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       {props.data.map((expense) => (
         <div>
           <ExpenseItem
--- react-project/src/components/Expenses/ExpenseItem.js
+++ react-project/src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
@@ -9,27 +9,29 @@ const ExpenseItem = (props) => {
   //   console.log("Item deleted");
   // };
 
-  const [expense, setExpense] = useState(props.amount);
-  const ChangeExpense = () => {
-    setExpense("100");
-  };
+  // const [expense, setExpense] = useState(props.amount);
+  // const ChangeExpense = () => {
+  //   setExpense("100");
+  // };
 
-  const [title, setTitle] = useState(props.title);
-  const ChangeTitle = () => {
-    setTitle("Title Updated");
-  };
+  // const [title, setTitle] = useState(props.title);
+  // const ChangeTitle = () => {
+  //   setTitle("Title Updated");
+  // };
 
   return (
-    <Card className="expense-item" id="abc">
-      <ExpenseDate date={props.date}></ExpenseDate>
-      <div className="expense-item__description">
-        <h2 id="h2">{title}</h2>
-        <div className="expense-item__price">Rs {expense}</div>
-      </div>
+    <div>
+      <Card className="expense-item" id="abc">
+        <ExpenseDate date={props.date}></ExpenseDate>
+        <div className="expense-item__description">
+          <h2 id="h2">{props.title}</h2>
+          <div className="expense-item__price">Rs {props.amount}</div>
+        </div>
 
-      <button onClick={ChangeTitle}>Change Title</button>
-      <button onClick={ChangeExpense}>Change Expense</button>
-    </Card>
+        {/* <button onClick={ChangeTitle}>Change Title</button>
+      <button onClick={ChangeExpense}>Change Expense</button> */}
+      </Card>
+    </div>
   );
 };
 export default ExpenseItem;
