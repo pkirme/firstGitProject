113ab718c768c305a2d28006b9c54d29d10e4b5c
pkirme <kirmepriti@gmail.com>
2023-12-19 11:42:32 +0530

    Adding Project

---
+++ first-react-project/src/Components/Users/AddUser.js
@@ -0,0 +1,16 @@
+import React from "react";
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">Username</label>
+      <input type="text" id="username"></input>
+      <label htmlFor="age">Age(years)</label>
+      <input type="text" id="age"></input>
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+export default AddUser;
