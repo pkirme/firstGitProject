1897d74fc58675506c4b12ffd59583520e67439e
pkirme <kirmepriti@gmail.com>
2024-01-05 22:46:27 +0530

    Adding indivisual meals item and displaying them.

--- restaurant-website/src/components/Meals/Meals.js
+++ restaurant-website/src/components/Meals/Meals.js
@@ -2,19 +2,11 @@ import { Fragment } from "react";
 import MealsSummary from "./MealsSummary";
 import AvailableMeals from "./AvailableMeals";
 
-const Meals = (props) => {
-  console.log(props.data);
+const Meals = () => {
   return (
     <Fragment>
       <MealsSummary />
-      {props.data.map((meal) => (
-        <AvailableMeals
-          key={meal.id}
-          name={meal.name}
-          description={meal.description}
-          price={meal.price}
-        />
-      ))}
+      <AvailableMeals />
     </Fragment>
   );
 };
