13e41df9717930cf8f2cafa3f7aeffcdce9e69c7
pkirme <kirmepriti@gmail.com>
2024-01-05 15:24:31 +0530

    Adding Restaurant Summary

---
+++ restaurant-website/src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+    </Fragment>
+  );
+};
+export default Meals;
