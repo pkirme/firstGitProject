07e0dff93c702da55c56bc0bad86a2abb6033e7b
pkirme <kirmepriti@gmail.com>
2024-02-08 16:01:13 +0530

    Building the header of your restaurant app.

---
+++ resort-web/src/App.js
@@ -0,0 +1,9 @@
+import Header from "./componentes/Layout/Header";
+
+function App() {
+  return <>
+  <Header/>
+  </>;
+}
+
+export default App;
---
+++ resort-web/src/componentes/Layout/Header.js
@@ -0,0 +1,18 @@
+import mealImg from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>React Meal</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealImg} alt="" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
