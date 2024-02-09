14c54646cf3e0214c8ae0a4d5d8254427e651ca5
pkirme <kirmepriti@gmail.com>
2024-02-09 22:09:23 +0530

    Making a hard Coded Cart without any Functionality.

--- resort-web/src/App.js
+++ resort-web/src/App.js
@@ -1,9 +1,11 @@
+import Cart from "./componentes/Cart/Cart";
 import Header from "./componentes/Layout/Header";
 import Meals from "./componentes/Meals/Meals";
 
 function App() {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Meals />
