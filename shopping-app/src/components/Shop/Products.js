703e5a21072edb0aefbe42872ba8e4d34d0679dc
pkirme <kirmepriti@gmail.com>
2024-04-13 15:38:58 +0530

    Add to Cart Fixes

--- shopping-app/src/components/Shop/Products.js
+++ shopping-app/src/components/Shop/Products.js
@@ -1,16 +1,41 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const dummyData = [
+  {
+    id: "1",
+    title: "product1",
+    price: 100,
+    description: "This is a first product - amazing!",
+  },
+  {
+    id: "2",
+    title: "product2",
+    price: 200,
+    description: "This is a second product - amazing!",
+  },
+  {
+    id: "3",
+    title: "product3",
+    price: 300,
+    description: "This is a third product - amazing!",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {dummyData.map((item) => (
+          <ProductItem
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+          />
+        ))}
       </ul>
     </section>
   );
--- shopping-app/src/store/index.js
+++ shopping-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiReducer from "../store/ui";
+import cartReduser from "../store/cart";
 
 const store = configureStore({
   reducer: {
     ui: uiReducer,
+    cart: cartReduser,
   },
 });
 
